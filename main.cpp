596e467b58a757ed064228ef6beea5f33ea366fb
RedLexa <alexandrebrunoferreira6@hotmail.com>
2022-12-22 15:57:52 +0000

    started main

--- main.cpp
+++ main.cpp
@@ -3,9 +3,15 @@
 //
 #include <bits/stdc++.h>
 using namespace std;
-
+int V;
+int E;
+unsigned long long soluttion;
 void processInput(){
+    scanf("%d\n",V);
+    scanf("%d\n",E);
+    for(int i = 0; i < E;i++){
 
+    }
 }
 
 
