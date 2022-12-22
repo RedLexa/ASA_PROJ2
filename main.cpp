d0eb7543f05e7fbfee22e994335bb3b11c0e51d4
RedLexa <alexandrebrunoferreira6@hotmail.com>
2022-12-22 17:04:27 +0000

    commit process input

--- main.cpp
+++ main.cpp
@@ -2,15 +2,28 @@
 // Created by alexa on 22/12/2022.
 //
 #include <bits/stdc++.h>
+#include <map>
+#include <tuple>
+#include <vector>
+#include <list>
 using namespace std;
 int V;
 int E;
-unsigned long long soluttion;
+unsigned long long solution;
+vector<int> upper;
+map<tuple<int,int>,int> graph;
 void processInput(){
-    scanf("%d\n",V);
-    scanf("%d\n",E);
-    for(int i = 0; i < E;i++){
-
+    int a,b, w;
+    tuple <int,int> path;
+    scanf("%d\n",&V);
+    scanf("%d\n",&E);
+    for(int i = 1; i < V;i++) {
+        upper[i] = i;
+    }
+    for(int i = 1; i < E;i++){
+        scanf("%d %d %d\n",&a,&b,&w);
+        path = make_tuple(a,b);
+        graph.insert({path,w});
     }
 }
 
@@ -19,5 +32,6 @@ void processInput(){
 
 
 int main(){
+    processInput();
     return 0;
 }
